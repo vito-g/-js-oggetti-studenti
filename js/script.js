738e8e74c1bd79de37eff2a4a57c11e479457483
Vito Gentile <gentilevito1076@gmail.com>
2021-01-18 15:59:12 +0100

    Creazione Oggetto Student

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// TRACCIA:
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for tutte le proprietà.
+// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
+var student = {
+  'nome' : 'Sandro',
+  'cognome' : 'Giacomelli',
+  'età' : 25
+};
+console.log(student);
